e8b115072e0887a1edd1a50821c7d9782472500e
Scott Danzig <sdanzig@gmail.com>
2012-10-27 09:58:23 -0400

    Changed directory

---
+++ Nov1/Nov1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov1AppDelegate.h
+//  Nov1
+//
+//  Created by Scott Danzig on 10/27/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
