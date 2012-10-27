700a1fbc37ead22ad04585360cd55073bb1df612
Scott Danzig <sdanzig@gmail.com>
2012-10-27 09:56:49 -0400

    Project created

---
+++ Nov1/Nov1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov1
+//
+//  Created by Scott Danzig on 10/27/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Nov1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Nov1AppDelegate class]));
+    }
+}
